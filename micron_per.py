67da344a28ad8d8653fa89bc20ed4ba3e432ad1e
Caballoazul <jjajang71@gmail.com>
2026-06-23 08:19:12 +0900

    Update micron_per.py

--- micron_per.py
+++ micron_per.py
@@ -18,6 +18,17 @@ def get_micron_data():
         ticker = yf.Ticker("MU")
         info = ticker.info
 
+        hist = ticker.history(period="2d")
+
+        close_today = hist["Close"].iloc[-1]
+        close_yesterday = hist["Close"].iloc[-2]
+
+        change_pct = (
+            close_today / close_yesterday - 1
+        ) * 100
+
+
+        
         price = (
             info.get("currentPrice")
             or info.get("regularMarketPrice")
@@ -39,7 +50,8 @@ def get_micron_data():
         return {
             "name": "Micron",
             "ticker": "MU",
-
+            "change_pct": change_pct,
+            
             # 가격 지표
             "price": float(price) if price else 0,
 
