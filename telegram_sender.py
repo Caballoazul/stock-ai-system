794b8f46544dd015bd68a0c3f6aa85e751aa4299
Caballoazul <jjajang71@gmail.com>
2026-06-22 15:48:17 +0900

    Create telegram_sender.py

---
+++ telegram_sender.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+def send_telegram(message):
+
+    token = os.getenv("TELEGRAM_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    payload = {
+        "chat_id": chat_id,
+        "text": message
+    }
+
+    requests.post(url, data=payload)
